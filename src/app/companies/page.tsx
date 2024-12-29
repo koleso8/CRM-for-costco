46a565796bb234fcccc223845d12a38f2e5f2655
koleso8 <dev.koleso@gmail.com>
2024-12-29 16:27:32 +0200

    Update page.tsx

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -28,6 +28,33 @@ export default function page({ children }: PageProps) {
           country="Ukraine"
           joinedDate="05.12.2024"
         />
+        <CompanyRow
+          id={'2'}
+          category={'Product'}
+          company={'Pepsi-Co'}
+          status={Status.Active}
+          promotion={false}
+          country="USA"
+          joinedDate="11.02.2023"
+        />
+        <CompanyRow
+          id={'3'}
+          category={'Commerce'}
+          company={'A-Bank'}
+          status={Status.Suspended}
+          promotion={true}
+          country="Ukraine"
+          joinedDate="03.11.2024"
+        />
+        <CompanyRow
+          id={'4'}
+          category={'Market'}
+          company={'Basic'}
+          status={Status.NotActive}
+          promotion={false}
+          country="UK"
+          joinedDate="01.27.2022"
+        />
       </CompanyTable>
     </>
   );
