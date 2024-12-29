df4c8e2eb5bca6514055d80aa36837a9f7aafc77
koleso8 <dev.koleso@gmail.com>
2024-12-29 16:42:24 +0200

    fix routes

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../components/CompanyHeader';
-import Toolbar from '../components/Toolbar';
-import SearchInput from '../components/SearchInput';
-import AddCompanyButton from '../components/AddCompanyButton';
-import CompanyTable from '../components/CompanyTable';
-import CompanyRow from '../components/CompanyRow';
-import { Status } from '../components/StatusLabel';
+import Header from '../../components/CompanyHeader';
+import Toolbar from '../../components/Toolbar';
+import SearchInput from '../../components/SearchInput';
+import AddCompanyButton from '../../components/AddCompanyButton';
+import CompanyTable from '../../components/CompanyTable';
+import CompanyRow from '../../components/CompanyRow';
+import { Status } from '../../components/StatusLabel';
 
 export interface PageProps {
   children: React.ReactNode;
